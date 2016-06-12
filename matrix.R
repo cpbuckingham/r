19295370db9200fd1e25a528843d5b3359fc96cc
Cam <cameron.p.buckingham@gmail.com>
2016-06-12 12:11:30 -0500

    matrix

---
+++ matrix.R
@@ -0,0 +1,17 @@
+similar to a data frame but, 
+the entire thing needs to be the same
+type of data (mostly numeric)
+
+one <- matrix(1:100, nrow=10)
+one
+
+two <- matrix(51:60, nrow=2)
+two
+
+three <- matrix(61:70, nrow=2)
+three
+
+dim(two)
+dim(three)
+
+two*three
